0f91ee58e1ebb5c82c20bc78785ba16c49920da7
successdevop <successraphael28@gmail.com>
2024-02-23 11:57:41 +0100

    set up the applications boiler codes and importand folders and configurations needed for the Book Store Application

---
+++ Controllers/userController.js
@@ -0,0 +1,5 @@
+const registerUser = async (req, res) => {
+  res.send("Hello register");
+};
+
+module.exports = { registerUser };
---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const userRouter = require("./Routes/userRouter");
+
+// cors options
+const corsOptions = {
+  origin: "*",
+  credential: true,
+  optionSuccessStatus: 200,
+};
+// middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/users", userRouter);
+
+// app listening port
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    // database
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
